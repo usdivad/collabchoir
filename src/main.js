423ba56a3ba698ba0f0a55f0d0a9652a6d86057a
David Su <david.d.su@gmail.com>
2014-02-08 02:05:31 -0500

    jq

--- src/main.js
+++ src/main.js
@@ -3,16 +3,16 @@
 	//var synth = T("OscGen", {osc: })
 	var synth = T("pulse");
 	var mvmt = 0;
-	var display = document.getElementById("meter");
+	var display = $("#meter");
 	window.addEventListener("deviceorientation", handle_orientation, true);
 
 	function handle_orientation(event) {
 		mvmt = event.gamma + 90; //
-		display.innerHTML = "mvmt: " + mvmt.toFixed(2);
+		display.html("mvmt: " + mvmt.toFixed(2));
 		//console.log("a");
 	};
 
-	function 
+	//function 
 
 
 })();
