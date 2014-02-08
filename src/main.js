63e9fe121a6565d6867f89c949f9a39d91ac06c5
David Su <david.d.su@gmail.com>
2014-02-08 02:32:43 -0500

    sound going

--- src/main.js
+++ src/main.js
@@ -1,14 +1,37 @@
 //Main sheet
 (function() {
-	//var synth = T("OscGen", {osc: })
-	var synth = T("pulse");
-	var mvmt = 0;
-	var display = $("#meter");
+	var osc = T("square");
+	var env = T("adsr", {a:500, d:500, s:1, r:800});
+	//var synth = T("osc", {wave:"pulse", freq:880, mul:0.8});
+	var synth = T("OscGen", {osc:osc, env:env, mul:0.8, freq:880}).play();
+	console.log(synth);
+	var mvmt = 60;
+	var display_meter = $("#meter");
+	var display_io = $("#io");
+	var synth_on = false;
+
+	//Listeners
 	window.addEventListener("deviceorientation", handle_orientation, true);
+	$("#io").click(function() {
+		if (!synth_on) {
+			//synth.play();
+			synth.noteOn(mvmt, 100);
+			display_io.css("background-color", "green");
+			synth_on = true;
+			console.log(synth);
+		}
+		else {
+			//synth.pause();
+			synth.allNoteOff();
+			display_io.css("background-color", "black");
+			synth_on = false;
+		}
+		console.log(synth_on);
+	});
 
 	function handle_orientation(event) {
-		mvmt = event.gamma + 90; //
-		display.html("mvmt: " + mvmt.toFixed(2));
+		mvmt = (event.gamma + 90) * 100/180; //scale to 100%
+		display_meter.html("mvmt: " + mvmt.toFixed(2));
 		//console.log("a");
 	};
 
